f5ce560ade55b2ef50c702738ef7ea6cc40f4ed5
mohadese karimi <mohadesekariminik@gmail.com>
2026-07-26 20:10:37 +0430

    fix:use get_object_or_404 for category lookup

--- home/views.py
+++ home/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import get_object_or_404, render
 from django.views import View
-from unicodedata import category
-
 from orders.form import CartAddForm
 from .models import Product, Category
+
 
 class HomeView(View):
     def get(self, request, category_slug=None):
         products = Product.objects.filter(available=True)
         categories = Category.objects.filter(is_sub=False)
         if category_slug:
-            category = Category.objects.get(slug = category_slug)
+            category = get_object_or_404(Category, slug = category_slug)
             products = Product.objects.filter(category=category)
         return render(request, 'home/home.html', {'products': products , 'categories': categories})
 
